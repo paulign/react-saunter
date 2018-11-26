4197be043759dda783f2a5dda2acfffe0648dd39
Paul Ignatiev <vo100193ipa@gmail.com>
2018-11-26 08:55:42 +0200

    hide google maps api key

--- src/components/containers/MapContainer.js
+++ src/components/containers/MapContainer.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import MapElement from '../UI/MapElement'
 
 class MapContainer extends Component {
+    
     render() {
         return ( 
             <MapElement 
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSaE3x4mMXAPEvjnpc2H8Ns41WvY0HD8c" 
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
                 mapElement={<div className="h-100"/>} 
                 loadingElement={<div className="h-100"/>} 
                 containerElement={<div className="h-100"/>}
